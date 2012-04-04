3e7080eb532cdd60d9d9f6a3135fdcf445ae5753
Volodymyr Rudyi <vladimir.rudoy@gmail.com>
2012-04-04 22:47:41 +0300

    Added libevent usage and callbacks

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
-CFLAGS = -Werror -Wall -std=c99
+
+LIBS = $(shell pkg-config --libs libevent)
+CFLAGS = $(shell pkg-config --cflags libevent)
+CFLAGS += -Werror -Wall -std=c99
+
 
 SOURCES = src/snake-player.c src/snake-section.c src/main.c src/snake-field.c src/snake-game.c src/snake-server.c
 all: $(SOURCES)
-	gcc $(CFLAGS) $(SOURCES) -o bin/snaked
+	gcc $(CFLAGS) $(SOURCES) $(LIBS) -o bin/snaked
 
 clean:
 	rm bin/snaked
--- src/snake-server.c
+++ src/snake-server.c
@@ -16,15 +16,31 @@
  */
  
 #include "snake-server.h"
- 
-void sigio_handler(int sig_no);
- 
+
+void setnblock(int fd);
+void terminate_handler(int sig);
+
+void accept_cb(int fd, short ev, void *arg);
+void buffer_write_cb(struct bufferevent *event, void *arg);
+void buffer_read_cb(struct bufferevent *event, void *arg);
+void buffer_error_cb(struct bufferevent *event, short what,
+	void *arg);
+	
+struct ClientData
+{
+	struct bufferevent *buffer_event;
+	int socket;
+};
+
+int sock; 
 int server_main(const char *host, int port, const char *server_name)
 {
-	int sock;
+	int reuse = 1;
     struct sockaddr_in server_addr;
+    struct event accept_event;
     
 	SNAKE_DEBUG("Server launched");
+	event_init();
 
     if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
     {
@@ -43,17 +59,93 @@ int server_main(const char *host, int port, const char *server_name)
         SNAKE_ERROR("Can't bind");
         exit(EXIT_FAILURE);
     }
+    
+    if (listen(sock, 5) < 0)
+    {
+      SNAKE_ERROR("Can't listen");
+      return 1;
+    }
+
+	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuse,
+		sizeof(reuse));
+
+	setnblock(sock);
+	
+	event_set(&accept_event, sock, EV_READ|EV_PERSIST, accept_cb,
+            NULL);
+	event_add(&accept_event, NULL);
+	event_dispatch();
 
     SNAKE_DEBUG("Entering event loop. Waiting for clients on port %d", 
 		port);
-		
+	
     close(sock);
-    
     return 0;
 }
 
-void sigio_handler(int sig_no)
+void accept_cb(int fd, short ev, void *arg)
 {
-    SNAKE_DEBUG("Hi there! Accepted data from client");
+	int client_fd;
+	struct sockaddr_in client_addr;
+	socklen_t client_len = sizeof(client_addr);
+	struct ClientData *client;
+	SNAKE_DEBUG("Accepted connection");
+	
+
+
+	client_fd = accept(fd, (struct sockaddr *)&client_addr, 
+		&client_len);
+	if (client_fd < 0)
+	{
+		SNAKE_WARNING("Accept failed"); 
+		return;
+	}
+
+	setnblock(client_fd);
+
+	client = calloc(1, sizeof(*client));
+	if (client == NULL)
+	{
+		SNAKE_ERROR("Failed to allocate memory for client data");
+		return;
+	}
+		
+	client->socket = client_fd;
+	client->buffer_event = bufferevent_new(client_fd,
+		buffer_read_cb,
+		buffer_write_cb,
+		buffer_error_cb,
+		client
+	);
+
+	bufferevent_enable(client->buffer_event, EV_READ);
+}
+
+void buffer_read_cb(struct bufferevent *event, void *arg)
+{
+}
+
+void buffer_write_cb(struct bufferevent *event, void *arg)
+{
+}
+
+void buffer_error_cb(struct bufferevent *event, short what,
+	void *arg)
+{
+
+}
+
+void terminate_handler(int sig)
+{
+	close(sock);
+	exit(EXIT_SUCCESS);
+}
+
+void setnblock(int fd)
+{
+	int flags;
+	flags = fcntl(fd, F_GETFL);
+	flags |= O_NONBLOCK;
+	fcntl(fd, F_SETFL, flags);
 }
 
